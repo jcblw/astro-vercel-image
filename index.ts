00ef48573c125370b1374ad69e4e43f39cb36794
Jacob Lowe <jacoblowe2.0@gmail.com>
2023-02-10 09:23:40 -0800

    fix path resolution for outDir

--- index.ts
+++ index.ts
@@ -21,31 +21,39 @@ interface AstroVercelImagesConfig {
   contentSecurityPolicy?: string;
 }
 
+export const getVercelOutput = (root: URL) =>
+  new URL("./.vercel/output/", root);
+
 export default function createIntegration(
   config: AstroVercelImagesConfig = {
     sizes: [640, 750, 828, 1080, 1200],
     domains: [],
   }
 ): AstroIntegration {
+  const directory = path.join(process.cwd(), "./.vercel/output");
   return {
     name: "astro-vercel-image",
     hooks: {
-      "astro:config:setup": ({ config, updateConfig }) => {
-        if (config.output === "static") {
-          // This integration only works with static builds
-          updateConfig({ outputDir: "./.vercel/output/static" });
-        } else {
+      "astro:config:setup": async ({ config, updateConfig }) => {
+        if (config.output !== "static") {
           throw new Error("This integration only works with static builds");
         }
+        const outDir = new URL("./static/", getVercelOutput(config.root));
+        updateConfig({
+          build: {
+            ...config.build,
+            format: "directory",
+          },
+          outDir,
+        });
       },
       "astro:build:done": async () => {
         let existingConfig: { images?: AstroVercelImagesConfig } = {};
-        const directory = path.join(process.cwd(), "./.vercel/output");
-        const configPath = path.join(directory, "./config.json");
+        const configPath = path.resolve(directory, "./config.json");
         try {
           existingConfig = JSON.parse(await fs.readFile(configPath, "utf8"));
         } catch (e) {
-          console.log(e);
+          // console.log(e);
         }
         const newConfig = {
           version: 3,
